6a593ddf87eaa2d8bf2dbec43e55bc1f82ea2dc7
jtorresup <jotorres1@up.edu.ph>
2025-03-04 02:52:48 +0800

    models trained

--- forecast/apps.py
+++ forecast/apps.py
@@ -1,6 +1,11 @@
+import os
+
 from django.apps import AppConfig
 
 
-class RecommenderConfig(AppConfig):
+class ForecastConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'forecast'
+
+    def ready(self):
+        os.environ["KERAS_BACKEND"] = "torch"
