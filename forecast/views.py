d53bb3095ec3ac69faa902425f6f382f4ab38b6f
jtorresup <jotorres1@up.edu.ph>
2025-03-04 19:42:27 +0800

    we predicting

--- forecast/views.py
+++ forecast/views.py
@@ -1,9 +1,13 @@
-from rest_framework import viewsets
-from rest_framework.decorators import action
+from rest_framework import viewsets, status
+from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
+from rest_framework.request import Request
 
 from forecast.models import CommodityForecaster
-from forecast.serializers import CommodityForecasterSerializer
+from forecast.serializers import (
+    CommodityForecasterSerializer, PredictionRequestSerializer
+)
+from forecast.ml.predict import predict_best_day
 
 
 class CommodityForecasterViewSet(viewsets.ReadOnlyModelViewSet):
@@ -19,12 +23,40 @@ class CommodityForecasterViewSet(viewsets.ReadOnlyModelViewSet):
             .values_list(column, flat=True)
         )
 
-    @action(detail=False, methods=["get"])
-    def all_markets(self, request) -> Response:
+    @action(detail=False)
+    def all_markets(self, request):
         ms = self.query_column("market")
         return Response({"markets": list(ms)})
 
-    @action(detail=False, methods=["get"])
-    def all_commodities(self, request) -> Response:
+    @action(detail=False)
+    def all_commodities(self, request):
         cs = self.query_column("commodity")
         return Response({"commodities": list(cs)})
+
+
+@api_view(["get"])
+def get_predictions(request: Request) -> Request:
+    queries = PredictionRequestSerializer(data=request.GET)
+    if not queries.is_valid():
+        return Response(
+            {
+                "message": "Some query params are required.",
+                "errors": queries.errors,
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    queries = queries.validated_data
+
+    entries = CommodityForecaster.objects.filter(
+        market__in=queries["markets"], commodity__in=queries["commodities"],
+    )
+
+    objects = {market: {} for market in queries["markets"]}
+
+    for forecaster in entries:
+        best_day = predict_best_day(
+            forecaster.load_model(), queries["start_day"], queries["days"],
+        )
+        objects[forecaster.market][forecaster.commodity] = best_day
+
+    return Response(objects)
